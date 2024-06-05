fa7ae6142c125b02579e6e0fcab81a4e60539c2c
Rashid <isayevrjr@gmail.com>
2024-06-05 12:21:41 +0300

    Sprint05 Done

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,51 +1,16 @@
 import UIKit
 
-<<<<<<< HEAD
-final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
-=======
 final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, AlertPresenterProtocol {
->>>>>>> sprint_05
     
     //MARK: - Аутлеты
     
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
-<<<<<<< HEAD
-    @IBOutlet weak var yesButton: UIButton!
-    @IBOutlet weak var noButton: UIButton!
-    private var currentQuestionIndex = 0
-    private var correctAnswers = 0
-    
-    private let questionsAmount: Int = 10
-    private var questionFactory: QuestionFactoryProtocol = QuestionFactory()
-    private var currentQuestion: QuizQuestion?
-    
-    @IBAction private func yesButtonClicked(_ sender: Any) {
-        guard let currentQuestion = currentQuestion else {
-            return
-        }
-        let givenAnswer = true
-        yesButton.isEnabled = false
-        
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-    }
-    
-    @IBAction private func noButtonClicked(_ sender: Any) {
-        guard let currentQuestion = currentQuestion else {
-            return
-        }
-        let givenAnswer = false
-        noButton.isEnabled = false
-        
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-    }
-=======
     @IBOutlet private var yesButton: UIButton!
     @IBOutlet private var noButton: UIButton!
     
     // MARK: - Lifecycle
->>>>>>> sprint_05
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -63,29 +28,6 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD
-
-                let questionFactory = QuestionFactory()
-                questionFactory.setup(delegate: self)
-                self.questionFactory = questionFactory
-        
-        questionFactory.requestNextQuestion()
-    }
-    
-    // MARK: - QuestionFactoryDelegate
-    
-    func didReceiveNextQuestion(question: QuizQuestion?) {
-        guard let question = question else {
-            return
-        }
-        
-        currentQuestion = question
-        let viewModel = convert(model: question)
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.show(quiz: viewModel)
-        }
-=======
         statisticService = StatisticService()
         
         let questionFactory = QuestionFactory()
@@ -98,7 +40,6 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
         alertDelegate.alertController = self
         self.alertDelegate = alertDelegate
         
->>>>>>> sprint_05
     }
     
     // MARK: - QuestionFactoryDelegate
@@ -122,12 +63,8 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
         let questionStep = QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
             question: model.text,
-<<<<<<< HEAD
-            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
-=======
             questionNumber: "\(currentQuestionIndex + 1)/\(questionAmount)")
         
->>>>>>> sprint_05
         return questionStep
     }
     
@@ -137,29 +74,6 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
         counterLabel.text = step.questionNumber
     }
     
-<<<<<<< HEAD
-    private func show(quiz result: QuizResultsViewModel) {
-        let alert = UIAlertController(
-            title: result.title,
-            message: result.text,
-            preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
-            guard let self = self else { return }
-            
-            self.currentQuestionIndex = 0
-            self.correctAnswers = 0
-            
-            questionFactory.requestNextQuestion()
-        }
-        
-        alert.addAction(action)
-        
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-=======
->>>>>>> sprint_05
     private func showAnswerResult(isCorrect: Bool) {
         
         if isCorrect {
@@ -168,13 +82,6 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-<<<<<<< HEAD
-        imageView.layer.cornerRadius = 20
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            guard let self = self else { return }
-            
-=======
         imageView.layer.cornerRadius = 6
         
         // запускаем задачу через 1 секунду c помощью диспетчера задач
@@ -182,7 +89,6 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
             guard let self = self else {return}
             
            // код, который мы хотим вызвать через 1 секунду
->>>>>>> sprint_05
             self.showNextQuestionOrResults()
             self.noBorder()
             self.yesButton.isEnabled = true
@@ -244,26 +150,9 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
 
     //MARK: - Кнопки
     
-<<<<<<< HEAD
-    private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questionsAmount - 1 {
-            let text = correctAnswers == questionsAmount ?
-                    "Поздравляем, вы ответили на 10 из 10!" :
-                    "Вы ответили на \(correctAnswers) из 10, попробуйте ещё раз!"
-            let viewModel = QuizResultsViewModel(
-                title: "Этот раунд окончен!",
-                text: text,
-                buttonText: "Сыграть ещё раз")
-            show(quiz: viewModel)
-        } else {
-            currentQuestionIndex += 1
-            
-            questionFactory.self.requestNextQuestion() 
-=======
     @IBAction private func yesButtonClicked(_ sender: Any) {
         guard let currentQuestion else {
             return
->>>>>>> sprint_05
         }
         let givenAnswer = true
         
